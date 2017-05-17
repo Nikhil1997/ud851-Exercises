db5819d721f2c776e19453bf185510a920ee01a5
Nikhil1997 <mannavanikhil@gmail.com>
2017-05-17 20:36:39 +0530

    Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java

--- Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -19,10 +19,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView toyslist
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toyslist= (TextView) findViewById(R.id.name);
     }
 }
